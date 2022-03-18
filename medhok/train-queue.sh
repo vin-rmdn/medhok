d2166cc944da7410518bcc037745587bee178fa4
Alvin Januar <aljan_r@outlook.co.id>
2022-03-19 04:02:37 +0700

    Retrained CRNN

--- medhok/train-queue.sh
+++ medhok/train-queue.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 # Baseline - not available for MFCC
-python -u ./medhok/main.py train baseline mel_spectrogram 2>&1 | tee log/baseline-mel_spectrogram.txt
-python -u ./medhok/main.py train baseline spectrogram 2>&1 | tee log/baseline-spectrogram.txt
+# python -u ./medhok/main.py train baseline mel_spectrogram 2>&1 | tee log/baseline-mel_spectrogram.txt
+# python -u ./medhok/main.py train baseline spectrogram 2>&1 | tee log/baseline-spectrogram.txt
 
 # Chatfield et al. (2014)
 # python -u ./medhok/main.py train chatfield14 mel_spectrogram 2>&1 | tee log/chatfield14-mel_spectrogram.txt
@@ -20,8 +20,8 @@ python -u ./medhok/main.py train baseline spectrogram 2>&1 | tee log/baseline-sp
 # python -u ./medhok/main.py train warohma18 mfcc 2>&1 | tee log/warohma18-mfcc.txt
 
 # Draghici et al. (2020) - CRNN
-# python -u ./medhok/main.py train draghici20_crnn mel_spectrogram 2>&1 | tee log/draghici20_crnn-mel_spectrogram.txt
-# python -u ./medhok/main.py train draghici20_crnn spectrogram 2>&1 | tee log/draghici20_crnn-spectrogram.txt
+python -u ./medhok/main.py train draghici20_crnn mel_spectrogram 2>&1 | tee log/draghici20_crnn-mel_spectrogram.txt
+python -u ./medhok/main.py train draghici20_crnn spectrogram 2>&1 | tee log/draghici20_crnn-spectrogram.txt
 # python -u ./medhok/main.py train draghici20_crnn mfcc 2>&1 | tee log/draghici20_crnn-mfcc.txt
 
 echo Done!
